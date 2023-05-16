7971e1384b9727487171ae7b3511cbf94ccbcb83
branmous <brandon-377@hotmail.com>
2023-05-16 18:41:02 -0500

    Create Web Api events and Class library Shared

---
+++ SimGreenHearts/SimGreenHearts.Shared/Entities/EventType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimGreenHearts.Shared.Entities
+{
+    public class EventType
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Description { get; set; } = null!;
+    }
+}
