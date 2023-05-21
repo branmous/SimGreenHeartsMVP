2a5f24a8e77557cef6a6c3d2b16af53e051922fc
branmous <brandon-377@hotmail.com>
2023-05-21 12:44:44 -0500

    Init Desing Loign

---
+++ SimGreenHearts/SimGreenHearts.Shared/DTO/AuthDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimGreenHearts.Shared.DTO
+{
+    public class AuthDTO
+    {
+
+        [Display(Name = "Correo electrónico")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Debes ingresar un correo válido.")]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MinLength(6, ErrorMessage = "El campo {0} debe tener al menos {1} carácteres.")]
+        public string Password { get; set; } = null!;
+    }
+}
