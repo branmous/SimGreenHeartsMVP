e980ccc98992d8a6865c7a26480c44f7abdb509c
branmous <brandon-377@hotmail.com>
2023-05-23 18:30:33 -0500

    Generate Event List- event Create and implement error Boostrap

---
+++ SimGreenHearts/SimGreenHearts.Shared/DTO/EventDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimGreenHearts.Shared.DTO
+{
+    public class EventDTO
+    {
+        [Display(Name = "Observación")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Observation { get; set; } = null!;
+
+        [Display(Name = "Tipo de Evento")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debes seleccionar un {0}.")]
+        public int EventId { get; set; }
+    }
+}
