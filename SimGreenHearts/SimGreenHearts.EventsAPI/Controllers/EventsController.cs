87d2d65d272ca553c28c548d44c77da4d8d04202
Brandon Montoya <30757281+branmous@users.noreply.github.com>
2023-05-24 01:19:29 -0500

    Complete desing (#5)
    * Complete desing
    
    * Init view devices

--- SimGreenHearts/SimGreenHearts.EventsAPI/Controllers/EventsController.cs
+++ SimGreenHearts/SimGreenHearts.EventsAPI/Controllers/EventsController.cs
@@ -6,6 +6,7 @@ using SimGreenHearts.EventsAPI.Data;
 using SimGreenHearts.EventsAPI.Helpers;
 using SimGreenHearts.Shared.DTO;
 using SimGreenHearts.Shared.Entities;
+using SimGreenHearts.Shared.Enums;
 
 namespace SimGreenHearts.EventsAPI.Controllers
 {
@@ -32,11 +33,26 @@ namespace SimGreenHearts.EventsAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult> GetEvents()
+        public async Task<IActionResult> GetEvents()
         {
-            var events = await dataContext.Events.Include(e => e.User).Include(e => e.EventType).ToListAsync();
-            var eventList = new List<EventListDTO>();
+            var user = await userHelper.GetUserAsync(User.Identity!.Name!);
+            if (user == null)
+            {
+                return NotFound();
+            }
 
+            List<Event> events = new List<Event>();
+            if (user.UserType != UserType.Admin && user.UserType != UserType.Provider)
+            {
+                events = await dataContext.Events.Include(e => e.User).Include(e => e.EventType).Where(e => e.UserId == user.Id).ToListAsync();
+            }
+            else
+            {
+                events = await dataContext.Events.Include(e => e.User).Include(e => e.EventType).ToListAsync();
+
+            }
+
+            var eventList = new List<EventListDTO>();
             foreach (var item in events)
             {
                 eventList.Add(new EventListDTO
@@ -54,14 +70,11 @@ namespace SimGreenHearts.EventsAPI.Controllers
                 });
             }
 
-
-
-            return Ok(eventList);
-
+            return Ok(eventList.OrderByDescending(e => e.CreatedDate));
         }
 
         [HttpPost]
-        public async Task<ActionResult> PostEvent([FromBody] EventDTO eventDTO)
+        public async Task<IActionResult> PostEvent([FromBody] EventDTO eventDTO)
         {
             var user = await userHelper.GetUserAsync(User.Identity!.Name!);
             if (user == null)
