b9f287c9d1cbfb4581b55ea6318e2f7c377071fe
Brandon Montoya <30757281+branmous@users.noreply.github.com>
2023-05-25 10:09:47 -0500

    Feature/devices create pages (#6)
    * Complete create devices
    
    * Complete desing

--- SimGreenHearts/SimGreenHearts.IoTDevicesAPI/Interfaces/IAzureIotHubClient.cs
+++ SimGreenHearts/SimGreenHearts.IoTDevicesAPI/Interfaces/IAzureIotHubClient.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.Azure.Devices;
-using SimGreenHearts.IoTDevicesAPI.Models.DTO;
+using SimGreenHearts.Shared.DTO;
 
 namespace SimGreenHearts.IoTDevicesAPI.Interfaces
 {
     public interface IAzureIotHubClient
     {
-        Task<Device?> ChangeDeviceKeys(string hubName, string deviceId, UpdateDeviceKeysDto newKeys);
+        Task<Device?> ChangeDeviceKeys(string hubName, string deviceId, UpdateDeviceKeysDTO newKeys);
         Task<Device?> ChangeDeviceStatus(string hubName, string deviceId);
-        Task<Device?> CreateDevice(string hubName, CreateDeviceDto device);
+        Task<Device?> CreateDevice(string hubName, CreateDeviceDTO device);
         Task<bool> DeleteDevice(string hubName, string deviceId);
         Task<Device?> GetByDeviceId(string hubName, string deviceId);
         Task<List<Device?>> GetDevicesFromHub(string hubName);
---
+++ SimGreenHearts/SimGreenHearts.Shared/DTO/CreateDeviceDTO.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimGreenHearts.Shared.DTO
+{
+    public class CreateDeviceDTO
+    {
+        [Display(Name = "Identificación")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener entre {2} y {1} carácteres.")]
+        public string Id { get; set; } = string.Empty;
+
+        [Display(Name = "Estado del dispositivo")]
+        public int Status { get; set; }
+
+        [Display(Name = "Clave primaria")]
+        [StringLength(255, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener entre {2} y {1} carácteres.")]
+        public string? PrimaryKey { get; set; }
+
+
+        [Display(Name = "Clave secundaria")]
+        [StringLength(255, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener entre {2} y {1} carácteres.")]
+        public string? SecondaryKey { get; set; }
+
+
+        [Display(Name = "Estado de conexión")]
+        public int ConnexionStatus { get; set; }
+
+        [Display(Name = "Clave secundaria")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener entre {2} y {1} carácteres.")]
+        public string Latitude { get; set; } = string.Empty;
+
+        [Display(Name = "Clave secundaria")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0} debe tener entre {2} y {1} carácteres.")]
+        public string Longitude { get; set; } = string.Empty;
+    }
+}
