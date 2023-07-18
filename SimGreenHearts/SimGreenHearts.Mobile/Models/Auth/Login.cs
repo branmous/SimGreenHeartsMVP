7359e3784368fdc535eb3a178592dac499f49313
branmous <brandon-377@hotmail.com>
2023-07-17 19:17:08 -0500

    LoginView

---
+++ SimGreenHearts/SimGreenHearts.Mobile/Models/Auth/Login.cs
@@ -0,0 +1,11 @@
+﻿namespace SimGreenHearts.Mobile.Models.Auth
+{
+    public class Login
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsRenember { get; set; }
+    }
+}
